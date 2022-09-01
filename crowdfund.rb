f3ae5c8e6d418da1d049f29925e352231b6a51a2
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-01 09:50:39 -0400

    refactored to project_info method

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,3 +1,7 @@
+def project_info(project, funding = 0)
+  "#{project} has $#{funding} in funding."
+end
+
 project_1_name = "Project ABC"
 project_1_funding = 1000
 
@@ -7,6 +11,6 @@ project_2_funding = 2000
 project_3_name = "Project XYZ"
 project_3_funding = 500
 
-puts "#{project_1_name} has $#{project_1_funding} in funding."
-puts "#{project_2_name} has $#{project_2_funding} in funding."
-puts "#{project_3_name} has $#{project_3_funding} in funding."
+puts project_info(project_1_name, project_1_funding)
+puts project_info(project_2_name, project_2_funding)
+puts project_info(project_3_name, project_3_funding)
