b41c176e652c4fae321e687ddc938de008d40115
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-01 08:17:34 -0400

    added initial projects

---
+++ crowdfund.rb
@@ -0,0 +1,12 @@
+project_1_name = "Project ABC"
+project_1_funding = 1000
+
+project_2_name = "Project LMN"
+project_2_funding = 2000
+
+project_3_name = "Project XYZ"
+project_3_funding = 500
+
+puts "#{project_1_name} has $#{project_1_funding} in funding."
+
+puts "Projects:\n\t#{project_1_name}\n\t#{project_2_name}\n\t#{project_3_name}"
